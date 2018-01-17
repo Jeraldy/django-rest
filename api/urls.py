fffdfbe7bb046c49b0e0d1bc56ed3f0f29895c38
Deus Jeraldy <deusjeraldy@gmail.com>
2018-01-17 22:34:13 +0300

    added auth

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path,include
+from rest_framework.authtoken.views import obtain_auth_token
+from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path(r'', include('catalog.urls')),
+    url(r'^api-token-auth/', obtain_auth_token),
 ]
